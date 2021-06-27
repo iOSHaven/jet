6c295231945e69b0bd03be630ce4b57b10ec5c99
zeb <burroughszeb@me.com>
2021-06-27 15:55:27 -0500

    fix failing tests
    Laravel comes preconfigured with tests for user authentication and
    other registration processes. However,those features are not needed
    because the app will only allow authentication with Apple ID. xy

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -20,26 +20,18 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_authenticate_using_the_login_screen()
     {
-        $user = User::factory()->create();
-
-        $response = $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'password',
-        ]);
+        $response = $this->get('/auth/login/apple');
 
         $this->assertAuthenticated();
         $response->assertRedirect(RouteServiceProvider::HOME);
     }
 
-    public function test_users_can_not_authenticate_with_invalid_password()
+    public function test_users_cannot_post_login_route()
     {
-        $user = User::factory()->create();
-
-        $this->post('/login', [
-            'email' => $user->email,
+        $response = $this->post('/login', [
             'password' => 'wrong-password',
         ]);
 
-        $this->assertGuest();
+        $response->assertStatus(405);
     }
 }
